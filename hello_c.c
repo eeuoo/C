c119884ffcac838938ea5bcdbc544283c8938361
eeuoo <lee512joo@gmail.com>
2019-06-27 23:48:45 +0900

    2019-06-27

---
+++ hello_c.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define MAX 10
+
+int addNumber(int, int);    // addNumber() 함수의 프로토타입
+
+int main(void)   // 시작함수
+{
+
+// 변수 선언
+int a, num b;
+int result ; 
+
+// 변수 초기화
+a = 10 ;
+b = 20 ;
+
+printf('a + b = ');
+result = a + b;   // 덧셈연산자를 이용한 변수 연산
+
+printf('a + b = : %d\n', result);
+
+}
