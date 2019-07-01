1776e27d8c2bd1ebadf62e4163d4f431ab60aa01
eeuoo <lee512joo@gmail.com>
2019-07-01 23:31:27 +0900

    2019-07-01

---
+++ exam03_02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int person[3];  // 각 학생들의 국어 성적을 저장할 배열
+    int i;
+    int total_Korean = 0;
+    int avr_Korean = 0;
+
+    printf("학급의 국어 성적에 대한 총점과 평균 구하기\n");
+
+}
