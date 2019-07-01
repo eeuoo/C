b9b78ca8d1582b0dfa7a06b463f14b1dd993fb38
eeuoo <lee512joo@gmail.com>
2019-07-01 23:27:45 +0900

    2019-07-01

---
+++ exam03_01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+int num = 10;
+short age = 32;
+char grade_eng = "A";
+
+printf("num : %d\n", num);
+printf("Age : %d\n", age);
+printf("grade of english : %d\n", grade_eng);
+
+return 0;
+}
