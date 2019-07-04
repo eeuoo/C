4ff87bd436ed29e7a3e0fae551b0f6a71cacef66
eeuoo <lee512joo@gmail.com>
2019-07-04 23:32:56 +0900

    2019-07-04

---
+++ exam04_02.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    // 한 줄에 하나의 정수형 변수를 선언
+    int data_a;
+    int data_b;
+
+    // 같은 자료형은 컴마(,)로 구분하여 선언
+    int data_c, data_d ;
+
+    // 선언과 함께 초기화
+    int data_e = 10;
+
+    // 같은 데이터를 한번에 초기화
+    data_c = data_d = 100;
+
+    return 0;
+}
