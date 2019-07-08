e1684ead7fa5232998a2d0939d6f63a74932bf49
eeuoo <lee512joo@gmail.com>
2019-07-08 23:51:27 +0900

    2019-07-08

---
+++ exam05_01.c
@@ -0,0 +1,16 @@
+#include <stdint.h>
+
+int main()
+{
+    int a = 99;
+    int b = 2;
+
+    printf(" a + b의 결과 : %d\n", a + b);
+    printf(" a - b의 결과 : %d\n", a - b);
+    printf(" a * b의 결과 : %d\n", a * b);
+    printf(" a / b의 결과 : %d\n", a / b);
+    printf(" a %% b의 결과 : %d\n", a % b);
+    printf(" -a의 결과 : %d\n", -a);
+
+    return 0;
+}
