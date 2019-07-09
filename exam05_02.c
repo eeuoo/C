65bec2f0977cca46ae4c9794062de14575436b69
eeuoo <lee512joo@gmail.com>
2019-07-09 23:28:29 +0900

    2019-07-09

---
+++ exam05_02.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+    int b = 5;
+
+    printf(" a > b는 %d입니다.\n", a > b);
+    printf(" a < b는 %d입니다.\n", a < b);
+    printf(" a == b는 %d입니다.\n", a == b);
+    printf(" a != b는 %d입니다.\n", a != b);
+    
+    return 0;
+}
