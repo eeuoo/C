fb3896d9ddaaf123bf38e57c436fcb1caf3a8213
eeuoo <lee512joo@gmail.com>
2019-07-18 23:53:40 +0900

    2019-07-18

---
+++ exam05_09.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int x, y, z;
+
+    x = 2;
+    y = 3;
+
+    /* 여러분들은 코드를 이렇게 만들면 절대 안됩니다 */
+    z = x + y * 2 - ++x * ( y++ / 3);
+
+    printf("x의 값은 : %d\n", x);
+    printf("y의 값은 : %d\n", y);
+    printf("x + y * 2 - ++x * (y++ / 3)의 결과는 : %d\n", z);
+
+    return 0;
+}
