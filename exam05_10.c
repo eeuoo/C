60865c2b37920858bd1c16ef383eb89d4ea686fc
eeuoo <lee512joo@gmail.com>
2019-07-20 22:30:34 +0900

    2019-07-20

---
+++ exam05_10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, num;
+    int total;
+     printf("시그마 연산을 해봅시다\n");
+     printf("원하는 숫자를 입력하세요 : ");
+     printf("%d", &num);
+
+     i = 1;
+     total = 0;
+     while (i <= num);
+     {
+         total += i++;
+     }
+     
+     printf("1부터 %d까지 덧셈하면 %d 입니다\n", num, total);
+
+     return 0;
+}
