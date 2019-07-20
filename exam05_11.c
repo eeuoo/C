12fd899ceac9d508075d769083214210fec3aa50
eeuoo <lee512joo@gmail.com>
2019-07-20 22:34:52 +0900

    2019-07-20

---
+++ exam05_11.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, result;
+    int i, value_new, value1, value2;
+
+    printf("피보나치 수열을 구해봅시다 \n");
+    printf("몇 개를 구해볼까요?");
+    scanf("%d", &n);
+
+    i = 0; 
+    value1 = value2 = 1;
+    printf("%4d", value1);
+    printf("%4d", value2);
+
+    while (i <= n)
+    {
+        value_new = value1 + value2;   // 이전의 두 값을 덧셈하여 새로운 값을 생성
+        printf("%4d", value_new);
+        value2 = value1;
+        value1 = value_new;
+        i++;
+    }
+
+    printf("\n");
+    return 0;
+    
+}
