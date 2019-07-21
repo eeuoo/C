b278b405864107864914819b0e9632a4be3ec5f1
eeuoo <lee512joo@gmail.com>
2019-07-22 00:09:00 +0900

    2019-07-22

--- exam06_02.c
+++ exam06_02.c
@@ -11,4 +11,16 @@ int main()
     printf("\n");
     printf("키가 %d이시군요. \n", your_height);   // 변수 your_height에 저장되어 있는 값을 출력
     
+    // if 문의 조건이 참인 경우의 실행할 코드 블록
+    if (your_height > 175) {
+        printf("키가 크시네요.\n");
+        printf("평균 키보다 %d cm만큼 더 크시네요.", (your_height - 175));
+    }
+    // if 문의 조건이 거짓인 경우의 실행할 코드 블록
+    else {
+        printf("키가 큰 편은 아니시군요.\n");
+        printf("평균 키보다 %d cm만큼 작으시네요.", (175 - your_height));
+    }
+
+    return 0;
 }
