91f8fdc212aa29d792f350638b9d1978f8efc823
eeuoo <lee512joo@gmail.com>
2019-07-27 19:59:42 +0900

    2019-07-27

---
+++ exam06_08.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int sum, num;
+    int i;
+
+    printf("1부터 덧셈하는 프로그램입니다. \n");
+    printf("마지막 값을 입력하세요.\n");
+    printf("1부터 => ");
+    scanf("%d", &num);
+
+    printf("1부터 %d까지 덧셈합니다.\n", num);
+
+    sum = 0;
+    i = 1;  // 제어변수 i의 초기화
+}
