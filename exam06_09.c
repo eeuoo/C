e3ecca657c30ac1e5931989257cc5ba7f3d32af4
eeuoo <lee512joo@gmail.com>
2019-07-28 23:51:54 +0900

    2019-07-28

---
+++ exam06_09.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int sum, num;
+    int i;
+
+    printf("1부터 덧셈하는 프로그램입니다. \n");
+    printf("마지막 값을 입력하세요.\n");
+    printf("1부터 => ");
+    scanf("%d", &num);
+
+    printf("1부터 %d까지 덧셈합니다.\n", num);
+
+    sum = 0;
+    for(i = 1; i <= num; sum += i++);    // for 문에서 모든 연산을 처리함
+
+    printf("\n 1부터 %d까지의 총합은 %d입니다.", num, sum);
+
+    return 0;
+}
