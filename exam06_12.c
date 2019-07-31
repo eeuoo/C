9bc2735064788ca7eb1b5b14d59ac3fdaf357d19
eeuoo <lee512joo@gmail.com>
2019-08-01 00:42:54 +0900

    2019-08-01

--- exam06_12.c
+++ exam06_12.c
@@ -5,12 +5,12 @@ int main()
     int i, j;
     i = j = 0;
 
-    printf("break 문의 테스트입니다. \n", i);
+    printf("break 문의 테스트입니다. \n");
 
     do
     {
-       printf("do - while 문의 %d번째 반복입니다. \n");
-        for(j = 0; j < 10; j++);{
+       printf("do - while 문의 %d번째 반복입니다.\n", i);
+        for(j = 0; j < 10; j++){
         if(j==5)
             break;    // 반복문을 종료하는 역할을 하는 break 문
             printf("for 문의 %d번째 반복입니다.\n", j);
--- exam06_13.c
+++ exam06_13.c
@@ -13,7 +13,7 @@ int main()
         
         for ( j = 0; j < 10; j++){
             if(j == 5)
-                continue  // continue문을 사용합니다.
+                continue;  // continue문을 사용합니다.
             
             printf("for문의 %d번째 반복입니다.\n", j);
         }
@@ -23,6 +23,6 @@ int main()
     } while (i < 5);
     
     printf("프로그램을 종료합니다.");
-    
+
     return 0;
 }
