1d2587bd55f5893d80294474b2d018a3da5c9c9e
eeuoo <lee512joo@gmail.com>
2019-08-07 23:58:34 +0900

    2019-08-07

---
+++ exam07_06_01.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+// 다른 파일에서 선언된 Display_gugudan() 함수를 외부 함수로 선언
+extern void Dispaly_gugudan(void);
+
+// 다른 파일에서 선언된 변수 num를 외부 변수로 선언
+extern int num;
+int flag;
+
+int main()
+{
+    flag = 0;
+    printf("구구단 출력 프로그램입니다. \n");
+
+    while (!flag)
+    {
+        printf("몇 단을 출력할까요? -> ");
+        scanf("%d", flag);
+
+        if(num > 1 && num < 10)
+        {
+            flag = 1;
+            printf("%d단을 선택하셨군요.\n\n", num);
+            Dispaly_gugudan();
+        }
+        else 
+            printf("2부터 9까지만 가능합니다. 다시 입력해주세요. \n");
+    }
+    
+    return 0;
+}
