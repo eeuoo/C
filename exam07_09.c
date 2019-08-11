a93902f16e99b6ca295d98d0172339ee8ebf2145
eeuoo <lee512joo@gmail.com>
2019-08-12 00:05:53 +0900

    2019-08-12

--- exam07_09.c
+++ exam07_09.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void drawStar()
+void drawStar();
 
 int main()
 {
@@ -17,12 +17,12 @@ int main()
         drawStar();  // 입력 받은 행 수만큼 반복하여 drawStar() 함수를 호출함
 }
 
-void drawStart()
+void drawStar()
 {
     // 현재 행에 출력할 '*' 문자의 개수를 저장하는 정적 변수 number의 선언과 초기화
     static int number = 1;
     int i;
-
+    printf("%d", number);
     // 1부터 정적 변수 number까지 반복하여 '*'문자 출력
     for(i = 0; i < number; i++)
         printf("* ");
