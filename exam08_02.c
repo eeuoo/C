cc40278b9433a41d67aace6ef39fa1775886a494
eeuoo <lee512joo@gmail.com>
2019-08-13 23:36:09 +0900

    2019-08-13

--- exam08_02.c
+++ exam08_02.c
@@ -23,3 +23,22 @@ void main(void)
     DisplayTable();
 }
 
+void DisplayTable(void)
+{
+    int i;
+    int Total[2] = (0, 0);  // 영어 점수와 수학 점수의 총점을 저장하기 위한 배열 Total의 선언과 초기화
+
+    printf("학생 번호\t영어 점수\t수학 점수\n");
+
+    //각 학생 별로 영어 점수와 수학 점수를 출력합니다
+    for(i = 0; i < 5; i++) {
+        // 각 학생에 대한 영어 점수와 수학 점수의 출력
+        printf("\t%d\t\t%d\t\t%d\n", i+1, grade[i][0], grade[i][1]);
+        Total[0] += grade[i][0];   // 영어 총점을 계산합니다
+        Total[1] += grade[i][1];   // 수학 총점을 계산합니다
+    }
+
+    printf("=======================================\n");
+    printf("총 점\t\t\t%d\t\t%d\n", Total[0], Total[i]);  // 총점 출력
+    printf("평 균\t\t\t%d\t\t%d\n", Total[1] / 5, Total[1] / 5); // 평균 출력
+}
