413f20d15ab1e15351e80155acc7f793b6b1ee60
eeuoo <lee512joo@gmail.com>
2019-08-28 14:41:34 +0900

    2019-08-27

--- exam09_06.c
+++ exam09_06.c
@@ -21,12 +21,12 @@ void addr(int x)
 
     // 주어진 매개변수 x만큼 malloc()함수를 사용하여 메모리 할당
     ptr = (char*)malloc(sizeof(char)*x);
-    printf("\n할당된 메몰에 들어갈 값을 입력하시오\n");
+    printf("\n할당된 메모리에 들어갈 값을 입력하시오\n");
 
     // 반복문을 실행하면서 배열 ptr에 값 일겨
     while(i < x) {
         printf("ptr[%d] => ", i);
-        scanf("%d", &ptr[i]);
+        scanf("%s", &ptr[i]);
         i++;
     }
 
@@ -35,6 +35,6 @@ void addr(int x)
         sum = sum + ptr[i];
 
     // 포인터 ptr의 첫 번째 항목의 주소와 마지막 항목의 주소를 출력
-    printf("\n0x%x에서 0x%x까지 저장된 값들의 합은 %d 이다.", ptr, ptr+x, sum);
+    printf("\n0x%s에서 0x%s까지 저장된 값들의 합은 %d 이다.", ptr, ptr+x, sum);
     free(ptr);
 }
