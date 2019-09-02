9704953269bf0fcfcf0454a5f5eb1f5d29a2b767
eeuoo <lee512joo@gmail.com>
2019-09-03 00:08:06 +0900

    2019-09-03

--- section_005.c
+++ section_005.c
@@ -25,15 +25,24 @@ int main()
     // int for;
     // char kang
 
-    char aa = 'A';
-    printf("%c \n", aa);  // 문자 A
-    printf("%d \n", aa);  // 아스키 코드 65
+    // char aa = 'A';
+    // printf("%c \n", aa);  // 문자 A
+    // printf("%d \n", aa);  // 아스키 코드 65
 
-    char ab[6] = "KOREA"; // C언어에서 ""로 감싸진 건 문자열, 맨 마지막 널문자('\0')
-    printf("%s", ab);
+    // char ab[6] = "KOREA"; // C언어에서 ""로 감싸진 건 문자열, 맨 마지막 널문자('\0')
+    // printf("%s", ab);
 
     // short int si = 32768; // short는 최대 32767
     // int in = 32768;
     // float fl = 24.56f;    // 단정도 실수형 뒤에 f를 붙인다
     // double dfl = 24.5678; // 배정도 실수형
+
+    int i, j;
+    float aa;
+    char cc, dd[10];
+    scanf("%c \n", &cc);
+    scanf("%d %o\n", &i, &j);
+    scanf("%f", &aa);
+    scanf("%s", dd);
+
 }
