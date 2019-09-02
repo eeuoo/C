5172a0f4cea7943206293beb6c0a5bc6a530469d
eeuoo <lee512joo@gmail.com>
2019-09-02 23:55:34 +0900

    2019-09-02

--- section_005.c
+++ section_005.c
@@ -2,10 +2,17 @@
 
 int main()
 {
-    int i, j, k;
-    scanf("%d %d", &i, &j);
+    // int i, j, k;
+    // scanf("%d %d", &i, &j);
 
-    k = i + j;
+    // k = i + j;
 
-    printf("%d\n", k);
+    // printf("%d\n", k);
+
+    A = 3;
+    printf("%d\n", A);
+    A = 5 + 3;
+    printf("%d\n", A);
+    A = A + 5;
+    printf("%d\n", A);
 }
