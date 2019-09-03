3b55667d741707738741854867d6fe393961ed2f
eeuoo <lee512joo@gmail.com>
2019-09-04 00:28:29 +0900

    2019-09-04

--- section_005.c
+++ section_005.c
@@ -37,12 +37,22 @@ int main()
     // float fl = 24.56f;    // 단정도 실수형 뒤에 f를 붙인다
     // double dfl = 24.5678; // 배정도 실수형
 
-    int i, j;
-    float aa;
-    char cc, dd[10];
-    scanf("%c \n", &cc);
-    scanf("%d %o\n", &i, &j);
-    scanf("%f", &aa);
-    scanf("%s", dd);
+    // int i, j;
+    // float aa;
+    // char cc, dd[10];
+    // scanf("%c \n", &cc);
+    // scanf("%d %o\n", &i, &j);
+    // scanf("%f", &aa);
+    // scanf("%s", dd);
 
+    int i = 250;
+    float a = 125.23f;
+    float b = 314.1592e+5;
+    char c = 'A';
+    char dd[] = "Korea";
+
+    printf("10진수i = %d\n 8진수i = %o\n", i, i);
+    printf("a = %8.2f, b = %e\n", a, b);
+    printf("c값은 문자로 %c이고 아스키 코드로 %d이다.\n", c, c);
+    printf("%-10s, %10s\n\r", dd, dd);
 }
