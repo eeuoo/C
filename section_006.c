1990754fdd1b54d96b926f7e6478475284752bc4
eeuoo <lee512joo@gmail.com>
2019-09-06 00:01:56 +0900

    2019-09-06

--- section_006.c
+++ section_006.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 
-main()
+int main()
 {
-    int a = 10, even = 0, odd = 0;
-    for (int i = 1; i <= a; ++i) {
-        if (i % 2 == 0)
-            even += 1;
-        else
-            odd += 1;
-    }
+    // int a = 10, even = 0, odd = 0;
+    // for (int i = 1; i <= a; ++i) {
+    //     if (i % 2 == 0)
+    //         even += 1;
+    //     else
+    //         odd += 1;
+    // }
 
-    printf("%d, %d\n", even, odd);
+    // printf("%d, %d\n", even, odd);
+
+    int a = 15, b = 0;
+
+    if (a >10)
+        b = a - 10;
+
+    b = b + (10 - b);
+
+    printf("%d\n",b);
 }
