42cb66403a36cc6f8006a1bb489dfb63f34f4b54
eeuoo <lee512joo@gmail.com>
2019-09-06 00:26:05 +0900

    2019-09-06

--- section_006.c
+++ section_006.c
@@ -28,16 +28,37 @@ int main()
     //     cha = b - a;
     // printf("%d\n", cha);
 
-    int jum = 100;
-    if (jum >= 90) {
-        printf("학점은 A입니다.\n");
-    } else if (jum >= 80) {
-        printf("학점은 B입니다.\n");
-    } else if (jum >= 70) {
-        printf("학점은 C입니다.\n");
-    } else if (jum >= 60) {
-        printf("학점은 D입니다.\n");
-    } else {
-        printf("학점은 F입니다.\n");
-    } 
+    // int jum = 100;
+    // if (jum >= 90) {
+    //     printf("학점은 A입니다.\n");
+    // } else if (jum >= 80) {
+    //     printf("학점은 B입니다.\n");
+    // } else if (jum >= 70) {
+    //     printf("학점은 C입니다.\n");
+    // } else if (jum >= 60) {
+    //     printf("학점은 D입니다.\n");
+    // } else {
+    //     printf("학점은 F입니다.\n");
+    // } 
+
+    int jum = 85;
+    switch ( jum / 10)
+    {
+        case 10:
+        case 9:
+            printf("학점은 A입니다.\n");
+            break;
+        case 8:
+            printf("학점은 B입니다.\n");
+            break;
+        case 7:
+            printf("학점은 C입니다.\n");
+            break;
+        case 6:
+            printf("학점은 D입니다.\n");
+            break;
+        default:
+            printf("학점은 F입니다.\n");
+            break;
+    }
 }
