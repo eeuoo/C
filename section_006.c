9eb8ed05678448dc088d704661876164ee13fbb3
eeuoo <lee512joo@gmail.com>
2019-09-06 00:28:38 +0900

    2019-09-06

--- section_006.c
+++ section_006.c
@@ -41,24 +41,38 @@ int main()
     //     printf("학점은 F입니다.\n");
     // } 
 
-    int jum = 85;
-    switch ( jum / 10)
+    // int jum = 85;
+    // switch ( jum / 10)
+    // {
+    //     case 10:
+    //     case 9:
+    //         printf("학점은 A입니다.\n");
+    //         break;
+    //     case 8:
+    //         printf("학점은 B입니다.\n");
+    //         break;
+    //     case 7:
+    //         printf("학점은 C입니다.\n");
+    //         break;
+    //     case 6:
+    //         printf("학점은 D입니다.\n");
+    //         break;
+    //     default:
+    //         printf("학점은 F입니다.\n");
+    //         break;
+    // }
+
+    // int i, hap = 0;
+    // for (i = 1; i <= 10; ++i)
+    //     hap += i;
+    // printf("%d, %d\n", i, hap);
+
+    int i = 0, hap = 0;
+    while (i < 10)
     {
-        case 10:
-        case 9:
-            printf("학점은 A입니다.\n");
-            break;
-        case 8:
-            printf("학점은 B입니다.\n");
-            break;
-        case 7:
-            printf("학점은 C입니다.\n");
-            break;
-        case 6:
-            printf("학점은 D입니다.\n");
-            break;
-        default:
-            printf("학점은 F입니다.\n");
-            break;
+        i++;
+        hap += i;
     }
+    printf("%d, %d\n", i, hap);
+    
 }
