6f9e453b6c61b7adc28d88dec2900a1cb6329c9d
eeuoo <lee512joo@gmail.com>
2019-09-08 00:11:29 +0900

    2019-09-08

--- section_007.c
+++ section_007.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+struct sawon
+{
+    char name[10];
+    char jikwi[10];
+    int pay;
+};
+
+struct sawon data;
+
 int main()
 {
     // int a = 20;
@@ -25,14 +34,27 @@ int main()
     // for(i = 0; i < 5; i++)
     //     printf("%d", *(p+i));
 
-    int a[3][4];
-    int i, j, k = 0;
-    for(i = 0; i < 3; i++)
-    {
-        for(j = 0; j < 4; j++)
-        {
-            k++;
-            a[i][j] = k;
-        }
-    }
+    // int a[3][4];
+    // int i, j, k = 0;
+    // for(i = 0; i < 3; i++)
+    // {
+    //     for(j = 0; j < 4; j++)
+    //     {
+    //         k++;
+    //         a[i][j] = k;
+    //     }
+    // }
+
+    printf("이름 : ");
+    scanf("%s", data.name);
+    printf("직위 : ");
+    scanf("%s", data.jikwi);
+    printf("급여 : ");
+    scanf("%d", &data.pay);
+    
+    printf("\n\n");
+
+    printf("이름 : %s\n", data.name);
+    printf("직위 : %s\n", data.jikwi);
+    printf("급여 : %d\n", data.pay);
 }
