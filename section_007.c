628f4c91c6974a84be8f00cb30436a3ac41ccfed
eeuoo <lee512joo@gmail.com>
2019-09-07 23:45:50 +0900

    2019-09-07

--- section_007.c
+++ section_007.c
@@ -2,9 +2,26 @@
 
 int main()
 {
-    int a = 20;
-    int *b;
-    b = &a;
-    *b = *b + 10;
-    printf("%d %p %d\n", a, b, *b);
+    // int a = 20;
+    // int *b;
+    // b = &a;
+    // *b = *b + 10;
+    // printf("%d %p %d\n", a, b, *b);
+
+    // int a[5];
+    // int i;
+    // for(i = 0; i < 5; i++)
+    //     a[i] = i + 10;
+
+    // for(i = 0; i < 5; i++)
+    //     printf("%d", a[i]);
+
+    int a[5];
+    int i;
+    int *p;
+    for(i = 0; i < 5; i++)
+        a[i] = i + 10;
+    p = a;
+    for(i = 0; i < 5; i++)
+        printf("%d", *(p+i));
 }
