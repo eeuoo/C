cd73532ef0c74ef22a2bd7afb439fd4aa4ceb4ef
eeuoo <lee512joo@gmail.com>
2019-09-10 00:30:19 +0900

    2019-09-10

--- section_007.c
+++ section_007.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
+#include <time.h>
 
 struct sawon
 {
@@ -86,20 +87,29 @@ int main()
     // result = pf(i, j);
     // printf("%d", result);
 
-    printf("%f\n", pow(2, 3));
-    printf("%f\n", sqrt(25));
-    printf("%d\n", abs(-20));
-    char a[] = "ad";
-    char b[] = "ac";
-    printf("%d\n", strcmp(a, b));
-    strcpy(a, b);
-    printf("%d\n", strcmp(a, b));
-    char c[] = "123";
-    char d[] = "456";
-    printf("%f\n", atof(c) + atof(d));
-    char e[] = "12.3";
-    char f[] = "45.6";
-    printf("%f\n", atof(e) + atof(f));
+    // printf("%f\n", pow(2, 3));
+    // printf("%f\n", sqrt(25));
+    // printf("%d\n", abs(-20));
+    // char a[] = "ad";
+    // char b[] = "ac";
+    // printf("%d\n", strcmp(a, b));
+    // strcpy(a, b);
+    // printf("%d\n", strcmp(a, b));
+    // char c[] = "123";
+    // char d[] = "456";
+    // printf("%f\n", atof(c) + atof(d));
+    // char e[] = "12.3";
+    // char f[] = "45.6";
+    // printf("%f\n", atof(e) + atof(f));
+
+    int n = 0;
+    srand(time(NULL));
+    while (n != 1)
+    {
+        n = rand() % 6 + 1;
+        printf("%d\n", n);
+    }
+    
 }
 
 // void func(i,j)
