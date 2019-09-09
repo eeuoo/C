efe7c13bcd7ed9e303e10bccc74b1fc6469674aa
eeuoo <lee512joo@gmail.com>
2019-09-10 00:13:17 +0900

    2019-09-10

--- section_007.c
+++ section_007.c
@@ -9,7 +9,8 @@ struct sawon
 
 struct sawon data;
 
-void func(int i, int j);
+// void func(int i, int j);
+int func(int i, int j);
 
 int main()
 {
@@ -66,11 +67,13 @@ int main()
 
 }
 
-void func(i,j)
+// void func(i,j)
+int func(i, j)
 
 int i, j;
 {
     i *= 3;
     j /= 3;
     printf("%d, %d\n", i, j);
+    return i;
 }
