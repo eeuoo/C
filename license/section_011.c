fe1f26450125df5d1c10865adc34a786153aad7e
eeuoo <lee512joo@gmail.com>
2019-09-15 23:54:49 +0900

    2019-09-14

---
+++ license/section_011.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{   
+    int i, j, sw;
+    i = j = 0;
+    sw = 0;
+    do 
+    {
+        i++;
+        if (sw == 0)
+        {
+            j += i;
+            sw = 1;
+        }
+        else
+        {
+            j -= i;
+            sw = 0;
+        }
+        
+    } while(i < 100);
+
+    printf("%d", j)
+}
