830abb3585c9e8ec8a73589f77e05accacaf1f33
eeuoo <lee512joo@gmail.com>
2019-09-15 23:59:38 +0900

    2019-09-15

--- license/section_011.c
+++ license/section_011.c
@@ -21,24 +21,37 @@ int main()
         
     // } while(i < 100);
 
-    int i = 0, j = 0;
-    while (1)
+    // int i = 0, j = 0;
+    // while (1)
+    // {
+    //     i++;
+    //     j += i;
+    //     if(i >= 99)
+    //     {
+    //         printf("%d", j);
+    //         break;
+    //     }
+    //     else
+    //     {
+    //         i++;
+    //         j -= i;
+    //     }  
+    // }
+    
+    int i;
+    double j;
+    i = 0;
+    j = 1;
+    do
     {
         i++;
-        j += i;
-        if(i >= 99)
-        {
-            printf("%d", j);
-            break;
-        }
+        if(i % 2 == 0)
+            j *= i;
         else
-        {
-            i++;
-            j -= i;
-        }
+            j *= i * -1;
         
-    }
+    } while (i < 100);
     
 
-    printf("%d", j);
+    printf("%11.4e", j);
 }
