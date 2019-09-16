8b78c37c087f78fdeab73a58e92c42a17799af55
eeuoo <lee512joo@gmail.com>
2019-09-17 00:00:41 +0900

    2019-09-17

--- license/section_013.c
+++ license/section_013.c
@@ -2,15 +2,24 @@
 
 int main()
 {
-    int i, j, k;
-    i = 0;
-    j = 1;
-    k = 1;
+    // int i, j, k;
+    // i = 0;
+    // j = 1;
+    // k = 1;
+    // do
+    // {
+    //     i++;
+    //     j += i;
+    //     k += j;
+    // } while (i < 19);
+    // printf("%d", k);
+
+    int i = 0, j = 0, k = 0;
     do
     {
         i++;
         j += i;
         k += j;
-    } while (i < 19);
+    } while (i < 20);
     printf("%d", k);
 }
