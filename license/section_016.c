591e1df32a459d54d058bafab38c9263e3cf68ca
eeuoo <lee512joo@gmail.com>
2019-09-17 23:58:47 +0900

    2019-09-17

--- license/section_016.c
+++ license/section_016.c
@@ -2,28 +2,39 @@
 
 int main()
 {
-    int a, i, j;
-    scanf("%d", &a);
-    i = a - 1;
-    j = 2;
-    while (1)
-    {
-        if(j <= i)
-        {
-            if (a % j == 0)
-            {
-                printf("소수 아님");
-                break;
-            }
-            else
-                j++;
+    // int a, i, j;
+    // scanf("%d", &a);
+    // i = a - 1;
+    // j = 2;
+    // while (1)
+    // {
+    //     if(j <= i)
+    //     {
+    //         if (a % j == 0)
+    //         {
+    //             printf("소수 아님");
+    //             break;
+    //         }
+    //         else
+    //             j++;
             
-        }
-        else
-        {
-            printf("소수");
-            break;
-        }
-    }
+    //     }
+    //     else
+    //     {
+    //         printf("소수");
+    //         break;
+    //     }
+    // }
+
+    int a, j;
+    scanf("%d", &a);
+    j = 2;
+    while (a % j != 0)
+        j++;
+    if (a == j)
+        printf("소수");
+    else
+        printf("소수 아님");
+    
     
 }
