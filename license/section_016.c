ca7cf161d3dde983f1f307d7de6a6f320624d96f
eeuoo <lee512joo@gmail.com>
2019-09-17 23:56:16 +0900

    2019-09-17

---
+++ license/section_016.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, i, j;
+    scanf("%d", &a);
+    i = a - 1;
+    j = 2;
+    while (1)
+    {
+        if(j <= i)
+        {
+            if (a % j == 0)
+            {
+                printf("소수 아님");
+                break;
+            }
+            else
+                j++;
+            
+        }
+        else
+        {
+            printf("소수");
+            break;
+        }
+    }
+    
+}
