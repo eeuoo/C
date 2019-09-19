8a4a7a6b4205636dc7ca9008a9d62cf83c35460c
eeuoo <lee512joo@gmail.com>
2019-09-20 00:00:15 +0900

    2019-09-20

--- license/section_022.c
+++ license/section_022.c
@@ -4,12 +4,28 @@
 
 int main()
 {
-    int b, bb, c, mok, nmg = 0, i;
+    int b, bb, c, mok, nmg = 0, k, i;
     int a[10];
 
     scanf("%d", &b);
     bb = b;
     c = -1;
+
+    // do
+    // {
+    //     c++;
+    //     mok = b / 2;
+
+    //     nmg = b - mok * 2;
+    //     a[c] = nmg;
+    //     b = mok;
+
+    // } while (mok != 0);
+
+    // printf("%d \n", bb);
+
+    // for (i = c; i >= 0; i--)
+    //     printf("%d", a[i]);
 
     do
     {
@@ -17,14 +33,16 @@ int main()
         mok = b / 2;
 
         nmg = b - mok * 2;
-        a[c] = nmg;
+        a[9-c] = nmg;
+
         b = mok;
 
     } while (mok != 0);
-
-    printf("%d \n", bb);
-
-    for (i = c; i >= 0; i--)
-        printf("%d", a[i]);
     
+    printf("%d\n", bb);
+
+    k = 9 - c;
+
+    for (i = k; i < 10; i++)
+        printf("%d", a[i]);
 }
