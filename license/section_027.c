89d78d1a0918ea0d4a2d30864f99ddfe552bcab9
eeuoo <lee512joo@gmail.com>
2019-09-21 23:56:42 +0900

    2019-09-21

--- license/section_027.c
+++ license/section_027.c
@@ -6,33 +6,57 @@
 
 int main()
 {
-    int i, j, k, L, m;
-    int a[10];
+    // int i, j, k, L, m;
+    // int a[10];
 
-    i = -1;
+    // i = -1;
+
+    // do
+    // {
+    //     i++;
+    //     scanf("%d", &a[i]);
+
+    // } while (i < 9);
+
+    // j = 9;
+
+    // for (k = 0; k <= 9; k++)
+    // {
+    //     if (a[k] >= FIND)
+    //         L = a[k] - FIND;
+    //     else
+    //         L = FIND - a[k];
+
+    //     if (L <= j)
+    //     {
+    //         j = L;
+    //         m = a[k];
+    //     }
+    // }
+    
+    // printf("%d", m);
+
+    int m, n, x, s;
+    int k[10] = {1,2,3,4,7,6,8,9,3,6};
+
+    m = 9;
+    n = -1;
 
     do
     {
-        i++;
-        scanf("%d", &a[i]);
-
-    } while (i < 9);
-
-    j = 9;
-
-    for (k = 0; k <= 9; k++)
-    {
-        if (a[k] >= FIND)
-            L = a[k] - FIND;
+        n++;
+        if (k[n] < 5)
+            x = 5 - k[n];
         else
-            L = FIND - a[k];
+            x = k[n] - 5;
 
-        if (L <= j)
+        if (m > x)
         {
-            j = L;
-            m = a[k];
+            m = x;
+            s = k[n];
         }
-    }
+
+    } while (n < 9);
     
-    printf("%d", m);
+    printf("%d", s);
 }
