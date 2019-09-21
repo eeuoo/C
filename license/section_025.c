fd9912a1e12ab4c85f94557104729d5ec44e75cd
eeuoo <lee512joo@gmail.com>
2019-09-21 23:45:06 +0900

    2019-09-21

--- license/section_025.c
+++ license/section_025.c
@@ -2,24 +2,64 @@
 
 /* 최댓값, 최솟값 구하기 */
 
-int main ()
+// int main ()
+// {
+//     int i;
+//     int a[10];
+//     int j = -1, max = 0;
+
+//     do
+//     {
+//         j++;
+//         scanf("%d", &a[j]);
+
+//     } while (j < 9);
+    
+//     for (i = 0; i <= 9; i++)
+//     {
+//         if (a[i] > max)
+//             max = a[i];
+//     }
+
+//     printf("%d", max);
+// }
+
+
+/* 최댓값, 최솟값을 제외한 평점의 평균 구하기 */
+
+int main()
 {
-    int i;
-    int a[10];
-    int j = -1, max = 0;
+    int m, min, max, hap, avg, i;
+    int a[7];
+
+    m = -1;
 
     do
     {
-        j++;
-        scanf("%d", &a[j]);
+        m++;
+        scanf("%d", &a[m]);
 
-    } while (j < 9);
-    
-    for (i = 0; i <= 9; i++)
+    } while (m < 6);
+
+    min = a[0];
+    max = a[0];
+    hap = a[0];
+    i = 0;
+
+    while (i < 6)
     {
+        i++;
+        hap += a[i];
+        if (a[i] < min)
+            min = a[i];
         if (a[i] > max)
             max = a[i];
     }
 
-    printf("%d", max);
+    hap = hap - min - max;
+    avg = hap / 5;
+
+    printf("%d %d %d %d", min, max, hap, avg);
+    
+    
 }
