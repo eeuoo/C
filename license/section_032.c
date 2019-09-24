5ce5c3edc6f3760b9122b8caf34183796f3bd5de
eeuoo <lee512joo@gmail.com>
2019-09-25 00:00:33 +0900

    2019-09-25

--- license/section_032.c
+++ license/section_032.c
@@ -74,44 +74,90 @@
 
 /* 중간 종료 */
 
+// int main()
+// {
+//     int n, i, j, sw, cnt, k;
+//     int data[10];
+//     n = -1;
+
+//     do
+//     {
+//         n++;
+//         scanf("%d", &data[n]);
+
+//     } while (n < 9);
+
+//     cnt = 0;
+
+//     for (i = 1; i <= 9; i++)
+//     {
+//         sw = 0;
+
+//         for (j = 0; j <= (9 - i); j++)
+//         {
+//             if (data[j] > data[j + 1])
+//             {
+//                 k = data[j];
+//                 data[j] = data[j + 1];
+//                 data[j + 1] = k;
+//                 cnt++;
+//                 sw = 1;
+//             }
+//         }
+
+//         if (sw == 0)
+//             break;
+//     }
+
+//     printf("%d ", cnt);
+
+//     for (int x = 0; x <= 9; x++)
+//         printf("%d", data[x]);
+    
+// }
+
+
+/* 좌우로 번갈아 가면서 실행 */
+
 int main()
 {
-    int n, i, j, sw, cnt, k;
-    int data[10];
-    n = -1;
+    int n, i, shift, left, right, buf;
+    int d[10] = {8, 5, 6, 2, 4, 1, 3, 7, 9, 10};
 
-    do
+    n = 9;
+    left = 0;
+    right = n;
+    
+    while (left < right)
     {
-        n++;
-        scanf("%d", &data[n]);
-
-    } while (n < 9);
-
-    cnt = 0;
-
-    for (i = 1; i <= 9; i++)
-    {
-        sw = 0;
-
-        for (j = 0; j <= (9 - i); j++)
+        for (i = left; i <= right - 1; i++)
         {
-            if (data[j] > data[j + 1])
+            if (d[i] > d[i + 1])
             {
-                k = data[j];
-                data[j] = data[j + 1];
-                data[j + 1] = k;
-                cnt++;
-                sw = 1;
+                buf = d[i];
+                d[i] = d[i + 1];
+                d[i + 1] = buf;
+                shift = i;
             }
         }
 
-        if (sw == 0)
-            break;
+        right = shift;
+
+        for (i = right; i >= left + 1; i--)
+        {
+            if (d[i - 1] > d[i])
+            {
+                buf = d[i - 1];
+                d[i - 1] = d[i];
+                d[i] = buf;
+                shift = i;
+            }
+        }
+
+        left = shift;
     }
 
-    printf("%d ", cnt);
-
-    for (int x = 0; x <= 9; x++)
-        printf("%d", data[x]);
+    for (int x = 0; x < 10; x++)
+        printf("%d", d[x]);
     
 }
