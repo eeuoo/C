773dd12adc9c57aa1be7ad0a4f5a0e07d2f703b3
eeuoo <lee512joo@gmail.com>
2019-09-24 23:40:17 +0900

    2019-09-24

--- license/section_031.c
+++ license/section_031.c
@@ -2,40 +2,65 @@
 
 /* 선택 정렬 */
 
+// int main()
+// {
+//     int m, i, j, k, x;
+//     int data[10];
+//     m = -1;
+
+//     do
+//     {
+//         m++;
+//         scanf("%d", &data[m]);
+
+//     } while (m < 9);
+    
+//     i = -1;
+
+//     do
+//     {
+//         i++;
+//         j = i;
+
+//         do
+//         {
+//             j++;
+//             if (data[i] > data[j])
+//             {
+//                 k = data[i];
+//                 data[i] = data[j];
+//                 data[j] = k;
+//             }
+//         } while (j < 9);
+        
+//     } while (i < 8);
+
+//     for (x = 0; x <= 9; x++)
+//         printf("%d ", data[x]);
+    
+// }
+
 int main()
 {
     int m, i, j, k, x;
     int data[10];
-    m = -1;
 
-    do
-    {
-        m++;
+    for (m = 0; m <= 9; m++)
         scanf("%d", &data[m]);
 
-    } while (m < 9);
-    
-    i = -1;
-
-    do
+    for (i = 0; i <= 8; i++)
     {
-        i++;
-        j = i;
-
-        do
+        for (j = i + 1; j <= 9; j++)
         {
-            j++;
             if (data[i] > data[j])
             {
                 k = data[i];
                 data[i] = data[j];
                 data[j] = k;
             }
-        } while (j < 9);
-        
-    } while (i < 8);
+        }
+    }
 
     for (x = 0; x <= 9; x++)
-        printf("%d ", data[x]);
-    
+        printf("%d", data[x]);
 }
