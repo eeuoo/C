4acf094b2ca66f63566476771dfd00b978b5414f
eeuoo <lee512joo@gmail.com>
2019-09-24 23:55:36 +0900

    2019-09-24

--- license/section_032.c
+++ license/section_032.c
@@ -41,9 +41,42 @@
 //         printf("%d ", data[x]);
 // }
 
+// int main()
+// {
+//     int n, i, j, k;
+//     int data[10];
+//     n = -1;
+
+//     do
+//     {
+//         n++;
+//         scanf("%d", &data[n]);
+
+//     } while (n < 9);
+    
+//     for (i = 1; i <= 9; i++)
+//     {
+//         for (j = 0; j <= 9 - 1; j++)
+//         {
+//             if (data[j] > data[j + 1])
+//             {
+//                 k = data[j];
+//                 data[j] = data[j + 1];
+//                 data[j + 1] = k;
+//             }
+//         }
+//     }
+
+//     for (int x = 0; x < 10; x++)
+//         printf("%d ", data[x]);
+// }
+
+
+/* 중간 종료 */
+
 int main()
 {
-    int n, i, j, k;
+    int n, i, j, sw, cnt, k;
     int data[10];
     n = -1;
 
@@ -53,20 +86,32 @@ int main()
         scanf("%d", &data[n]);
 
     } while (n < 9);
-    
+
+    cnt = 0;
+
     for (i = 1; i <= 9; i++)
     {
-        for (j = 0; j <= 9 - 1; j++)
+        sw = 0;
+
+        for (j = 0; j <= (9 - i); j++)
         {
             if (data[j] > data[j + 1])
             {
                 k = data[j];
                 data[j] = data[j + 1];
                 data[j + 1] = k;
+                cnt++;
+                sw = 1;
             }
         }
+
+        if (sw == 0)
+            break;
     }
 
-    for (int x = 0; x < 10; x++)
-        printf("%d ", data[x]);
+    printf("%d ", cnt);
+
+    for (int x = 0; x <= 9; x++)
+        printf("%d", data[x]);
+    
 }
