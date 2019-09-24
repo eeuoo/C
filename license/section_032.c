4c30846994f4616d3dfdae6e570c0d9a7eae2702
eeuoo <lee512joo@gmail.com>
2019-09-24 23:50:50 +0900

    2019-09-24

--- license/section_032.c
+++ license/section_032.c
@@ -2,6 +2,45 @@
 
 /* 버블 정렬 */
 
+// int main()
+// {
+//     int n, i, j, k;
+//     int data[10];
+//     n = -1;
+
+//     do
+//     {
+//         n++;
+//         scanf("%d", &data[n]);
+
+//     } while (n < 9);
+    
+
+//     i = 0;
+
+//     do
+//     {
+//         i++;
+//         j = -1;
+
+//         do
+//         {
+//             j++;
+            
+//             if (data[j] > data[j + 1])
+//             {
+//                 k = data[j];
+//                 data[j] = data[j + 1];
+//                 data[j + 1] = k;
+//             }
+//         } while (j < 9 - i);
+        
+//     } while (i < 9);
+    
+//     for (int x = 0; x <= 9; x++)
+//         printf("%d ", data[x]);
+// }
+
 int main()
 {
     int n, i, j, k;
@@ -15,28 +54,19 @@ int main()
 
     } while (n < 9);
     
-
-    i = 0;
-
-    do
+    for (i = 1; i <= 9; i++)
     {
-        i++;
-        j = -1;
-
-        do
+        for (j = 0; j <= 9 - 1; j++)
         {
-            j++;
-            
             if (data[j] > data[j + 1])
             {
                 k = data[j];
                 data[j] = data[j + 1];
                 data[j + 1] = k;
             }
-        } while (j < 9 - i);
-        
-    } while (i < 9);
-    
-    for (int x = 0; x <= 9; x++)
+        }
+    }
+
+    for (int x = 0; x < 10; x++)
         printf("%d ", data[x]);
 }
