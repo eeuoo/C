7140a3271a2f1fe5261ab3b8746965a6eb7b19cd
eeuoo <lee512joo@gmail.com>
2019-09-26 00:08:08 +0900

    2019-09-26

--- license/section_035.c
+++ license/section_035.c
@@ -2,37 +2,78 @@
 
 /* 이분 검색 */
 
+// int main()
+// {
+//     int j, L, h, m;
+//     int data[10] = {8, 15, 35, 55, 60, 61, 70, 80, 92, 99};
+
+//     scanf("%d", &j);
+//     L = 0;
+//     h = 9;
+
+//     while (1)
+//     {
+//         if (L <= h)
+//         {
+//             m = (L + h) / 2;
+
+//             if (j == data[m])
+//             {
+//                 printf("%d %d", j, m + 1);
+//                 break;
+//             }
+
+//             if (j < data[m])
+//                 h = m - 1;
+//             else
+//                 L = m + 1;
+//         }
+//         else
+//         {
+//             printf("%d NOT FOUND", j);
+//             break;
+//         }
+//     }
+    
+// }
+
+/* 2차원 배열 */
 int main()
 {
     int j, L, h, m;
-    int data[10] = {8, 15, 35, 55, 60, 61, 70, 80, 92, 99};
+    int data[2][10] = {
+        {1,2,3,4,5,6,7,8,9,10},
+        {100,66,25,88,90,65,87,86,58,99}
+    }
 
     scanf("%d", &j);
+
     L = 0;
     h = 9;
 
     while (1)
     {
-        if (L <= h)
+        if (L + h) 
         {
             m = (L + h) / 2;
 
-            if (j == data[m])
+            if (j == data[0][m])
             {
-                printf("%d %d", j, m + 1);
+                printf("%d %d\n", j, data[1][m]);
                 break;
             }
 
-            if (j < data[m])
+            if (j < data[0][m])
                 h = m - 1;
             else
                 L = m + 1;
         }
         else
         {
-            printf("%d NOT FOUND", j);
+            printf("%d NOT FOUND!", j);
             break;
         }
+        
     }
     
 }
