e4ba73b63fef82f3ea4e2e0a92b6004e11abccfa
eeuoo <lee512joo@gmail.com>
2019-10-04 23:56:36 +0900

    2019-10-04

---
+++ license/section_053.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+/* 학급별 최대, 최소 체중 */
+
+struct member
+{
+    char ban[4];
+    int bunho, wgt;
+};
+
+
+int main()
+{
+    struct member data[100];
+
+    int flag, a, b, i, k;
+    char c[4];
+
+    printf("반 번호 무게를 입력하세요.\n 모두 입력하셨으면 '0 0 0'을 입력하세요.\n");
+
+    flag = 0;
+    i = k = -1;
+    
+}
