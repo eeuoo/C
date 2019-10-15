528feeb9b4d566781c01111b56773a4653f5d9fa
eeuoo <lee512joo@gmail.com>
2019-10-15 23:35:30 +0900

    2019-10-15

--- c_guide/exam10_04.c
+++ c_guide/exam10_04.c
@@ -4,10 +4,10 @@
 int main()
 {
     char array[5] = {'a', 'b', 'c', 'd', 'e'};
-    char buf[5] = 'abcde';
-    char *ptr = 'abcde';
+    char buf[5] = "abcde";
+    char *ptr = "abcde";
 
-    printf("array : %d, array : %s\n", strlen(array), array);
-    printf("buf : %d, buf : %s\n", strlen(buf), buf);
-    printf("ptr : %d, ptr : %s\n", strlen(ptr), prt);
+    printf("array : %lu, array : %s\n", strlen(array), array);
+    printf("buf : %lu, buf : %s\n", strlen(buf), buf);
+    printf("ptr : %lu, ptr : %s\n", strlen(ptr), ptr);
 }
