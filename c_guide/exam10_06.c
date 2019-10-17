435fe5ed77f0ec49a8e7b4551b2ef45a3cc3fd6e
eeuoo <lee512joo@gmail.com>
2019-10-17 23:47:23 +0900

    2019-10-16

---
+++ c_guide/exam10_06.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+
+void StringCopy(char *, char *);
+
+int main()
+{
+    char *ptr1 == "abcde" ;
+    char *ptr2;
+
+    printf("StringCopy() 함수 호출 전\n");
+    printf("ptr1 : %s\n", ptr1);
+
+    StringCopy(ptr2, ptr1);
+
+    printf("StringCopy() 함수 호출\n");
+    printf("ptr1 : %s\n", ptr1);
+    printf("ptr2 : %s\n", ptr2);
+}
+
+void StringCopy(char *dst, char *src)
+{
+    while (*src)
+    {
+        *dst++ = *src++;
+    }
+    
+}
