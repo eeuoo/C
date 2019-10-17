2f209d4e039009dd123c89619c95eacb56da31d5
eeuoo <lee512joo@gmail.com>
2019-10-18 00:08:40 +0900

    2019-10-18

--- c_guide/exam10_06.c
+++ c_guide/exam10_06.c
@@ -4,26 +4,48 @@
 
 void StringCopy(char *, char *);
 
+// int main()
+// {
+//     char *ptr1 == "abcde" ;
+//     char *ptr2;
+
+//     printf("StringCopy() 함수 호출 전\n");
+//     printf("ptr1 : %s\n", ptr1);
+
+//     StringCopy(ptr2, ptr1);
+
+//     printf("StringCopy() 함수 호출\n");
+//     printf("ptr1 : %s\n", ptr1);
+//     printf("ptr2 : %s\n", ptr2);
+// }
+
+// void StringCopy(char *dst, char *src)
+// {
+//     while (*src)
+//     {
+//         *dst++ = *src++;
+//     }
+    
+// }
+
 int main()
 {
-    char *ptr1 == "abcde" ;
-    char *ptr2;
+    char *ptr1 = "abcde";
+    char *ptr2 = (char *)malloc(strlen(ptr1) + 1);
 
     printf("StringCopy() 함수 호출 전\n");
     printf("ptr1 : %s\n", ptr1);
 
     StringCopy(ptr2, ptr1);
 
-    printf("StringCopy() 함수 호출\n");
+    printf("StringCopy() 함수 호출 후\n");
     printf("ptr1 : %s\n", ptr1);
     printf("ptr2 : %s\n", ptr2);
 }
 
-void StringCopy(char *dst, char *src)
+void StringCopy(char *dst, char *src) 
 {
-    while (*src)
-    {
+    while(*src)
         *dst++ = *src++;
-    }
-    
+    *dst = "\0";
 }
