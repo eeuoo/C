e4e3c1cf2770d0cf80679ccd26925e4dc6b133ed
eeuoo <lee512joo@gmail.com>
2019-10-21 00:04:42 +0900

    2019-10-21

--- c_guide/exam10_07.c
+++ c_guide/exam10_07.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// 오류가 있는 예
+// int main()
+// {
+//     char *pBuf, *iBuf;
+//     int i;
+
+//     printf("C 프로그래밍에서의 포인터를 이용한 메모리 할당 예제\n");
+
+//     pBuf = (char *)malloc(10);
+
+//     iBuf = pBuf;
+
+//     for (i = 0; i < 10; i++)
+//         *pBuf++ = i + 'A';
+
+//     pBuf = iBuf;
+
+//     for (i = 0; i < 10; i++)
+//         printf("%c ", *pBuf++);
+    
+//     free(pBuf);
+// }
+
 int main()
 {
     char *pBuf, *iBuf;
@@ -20,5 +43,5 @@ int main()
     for (i = 0; i < 10; i++)
         printf("%c ", *pBuf++);
     
-    free(pBuf);
+    free(iBuf);
 }
