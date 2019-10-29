c2288d9bbfc987219bae9effd9d266789d6c9cf6
eeuoo <lee512joo@gmail.com>
2019-10-30 00:24:01 +0900

    2019-10-30

--- c_guide/exam11_06.c
+++ c_guide/exam11_06.c
@@ -8,11 +8,11 @@ struct TEST_STRUCT {
 union TEST_UNION {
     int i_data;
     char c_data[4];
-}
+};
 
 int main()
 {
     printf("구조체와 공용체의 크기를 비교해 봅시다.\n");
-    printf("구조체 TEST_STRUCT의 크기 : %d bytes\n", sizeof(struct TEST_STRUCT));
-    printf("공용체 TEST_UNION의 크기 : %d bytes\n", sizeof(union TEST_UNION));
+    printf("구조체 TEST_STRUCT의 크기 : %lu bytes\n", sizeof(struct TEST_STRUCT));
+    printf("공용체 TEST_UNION의 크기 : %lu bytes\n", sizeof(union TEST_UNION));
 }
