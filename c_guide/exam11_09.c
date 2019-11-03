f54731f20ea34e4ee466c0aef13c071dacf64605
eeuoo <lee512joo@gmail.com>
2019-11-03 22:16:55 +0900

    2019-11-03

--- c_guide/exam11_09.c
+++ c_guide/exam11_09.c
@@ -6,7 +6,7 @@ typedef struct _Lang {
     int Eng;
 } Lang;
 
-typeof struct _Student {
+typedef struct _Student {
     char name[20];
     Lang Language;
     int Society;
@@ -22,20 +22,20 @@ int main()
     strcpy(Persons[0].name, "반 점수");
     Persons[0].Language.Kor = 90;
     Persons[0].Language.Eng = 95;
-    persons[0].Society = 88;
+    Persons[0].Society = 88;
     Persons[0].Music = 80;
     Persons[0].Sum = Persons[0].Language.Kor + Persons[0].Language.Eng + Persons[0].Society + Persons[0].Music;
     Persons[0].Average = Persons[0].Sum / (float)4;
 
-    print("본인의 이름을 적으세요 : ");
+    printf("본인의 이름을 적으세요 : ");
     scanf("%s", Persons[1].name);
-    print("본인의 국어점수을 적으세요 : ");
+    printf("본인의 국어점수을 적으세요 : ");
     scanf("%d", &Persons[1].Language.Kor);
-    print("본인의 영어점수을 적으세요 : ");
+    printf("본인의 영어점수을 적으세요 : ");
     scanf("%d", &Persons[1].Language.Eng);
-    print("본인의 사회점수을 적으세요 : ");
+    printf("본인의 사회점수을 적으세요 : ");
     scanf("%d", &Persons[1].Society);
-    print("본인의 음악점수을 적으세요 : ");
+    printf("본인의 음악점수을 적으세요 : ");
     scanf("%d", &Persons[1].Music);
 
     Persons[1].Sum = Persons[1].Language.Kor + Persons[1].Language.Eng +  Persons[1].Society +  Persons[1].Music;
