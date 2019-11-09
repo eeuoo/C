7e6d459d6592bb570343f97e7fa694405d96886e
eeuoo <lee512joo@gmail.com>
2019-11-10 00:01:57 +0900

    2019-11-10

--- c_guide/exam12_07.c
+++ c_guide/exam12_07.c
@@ -12,8 +12,38 @@ int main(int argc, char* argv[])
         return -1;
     }
 
+    if ( (fin2 = fopen(argv[1], "rt")) == NULL ) {
+        printf("읽어들일 파일이 없습니다.\n");
+        return -1;
+    }
+
     if ( (fin2 = fopen(argv[2], "rt")) == NULL ) {
         printf("읽어들일 파일이 없습니다.\n");
         return -1;
     }
+
+     if ( (fin2 = fopen(argv[3], "wt")) == NULL ) {
+        printf("출력할 파일을 생성할 수 없습니다. \n");
+        return -1;
+    }
+
+
+    while (!feof(fin1) || !feof(fin2))
+    {
+        if (!feof(fin1)) {
+            fgets(src1, 80, fin1);
+            fputs(src1, fout);
+        }
+
+        if (!feof(fin2)) {
+            fgets(src1, 80, fin1);
+            fputs(src1, fout);
+        }
+    }
+
+    fclose(fin1);
+    fclose(fin2);
+    fclose(fout);
+
+    return 0;
 }
