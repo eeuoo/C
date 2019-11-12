96b23712ce32cccaba40ea8173344638047b24bb
eeuoo <lee512joo@gmail.com>
2019-11-12 23:59:58 +0900

    2019-11-12

--- c_guide/exam13_01.c
+++ c_guide/exam13_01.c
@@ -9,5 +9,7 @@ vodi main(void)
     print("여기는 디버깅을 위한 코드들입니다.\n");
     print("릴리즈 할 때는 실행되지 않습니다.\n");
     print("실행은 물론 컴파일도 되지 않습니다.\n");
+    #else
+    printf("여기는 릴리즈를 위한 코드들입니다.\n");
     #endif // DEBUG
 }
